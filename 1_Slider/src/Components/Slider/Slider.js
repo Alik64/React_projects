b8d5dcd2c11042bb744e51be45a5744c53e168cc
Albert <albert.barsamov@gmail.com>
2021-11-17 22:21:34 +0100

    use state slide anim

--- 1_Slider/src/Components/Slider/Slider.js
+++ 1_Slider/src/Components/Slider/Slider.js
@@ -3,5 +3,10 @@ import "./Slider.css";
 import dataSlider from "./dataSlider";
 
 export default function Slider(props) {
+  const [slideAnim, setSlideAnim] = useState({
+    index: 1,
+    inProgress: false,
+  });
+
   return <div></div>;
 }

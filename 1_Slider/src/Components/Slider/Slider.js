286749c7cd1e8167eff81a5b2a457036469ad8d0
Albert <albert.barsamov@gmail.com>
2021-11-18 11:01:48 +0100

    integration les points

--- 1_Slider/src/Components/Slider/Slider.js
+++ 1_Slider/src/Components/Slider/Slider.js
@@ -10,40 +10,74 @@ export default function Slider(props) {
   });
 
   const nextSlide = () => {
-    if (slideAnim.index !== dataSlider.length) {
-      setSlideAnim({ index: slideAnim.index + 1, inProgress: true });
-    } else if (slideAnim.index === dataSlider.length) {
-      setSlideAnim({ index: 1, inProgress: true });
+
+    if (slideAnim.index !== dataSlider.length && !slideAnim.inProgress) {
+
+      setSlideAnim({ index: slideAnim.index + 1, inProgress: true })
+
+      setTimeout(() => {
+        setSlideAnim({ index: slideAnim.index + 1, inProgress: false })
+      }, 400)
+
     }
-  };
+    else if (slideAnim.index === dataSlider.length && !slideAnim.inProgress) {
+
+      setSlideAnim({ index: 1, inProgress: true })
+
+      setTimeout(() => {
+        setSlideAnim({ index: 1, inProgress: false })
+      }, 400)
+    }
+
+  }
 
   const prevSlide = () => {
-    if (slideAnim.index !== 1) {
-      setSlideAnim({ index: slideAnim.index - 1, inProgress: true });
-    } else if (slideAnim.index === 1) {
-      setSlideAnim({ index: dataSlider.length, inProgress: true });
+
+    if (slideAnim.index !== 1 && !slideAnim.inProgress) {
+
+      setSlideAnim({ index: slideAnim.index - 1, inProgress: true })
+
+      setTimeout(() => {
+        setSlideAnim({ index: slideAnim.index - 1, inProgress: false })
+      }, 400)
     }
-  };
+    else if (slideAnim.index === 1 && !slideAnim.inProgress) {
+
+      setSlideAnim({ index: dataSlider.length, inProgress: true })
+
+      setTimeout(() => {
+        setSlideAnim({ index: dataSlider.length, inProgress: false })
+      }, 400)
+    }
+  }
+  const moveDot = index => {
+    setSlideAnim({ index: index, inProgress: false })
+  }
+
   return (
     <div className="container-slider">
       {dataSlider.map((obj, index) => {
         return (
           <div
             key={obj.id}
-            className={
-              slideAnim.index === index + 1 ? "slide active-anim" : "slide"
-            }
+            className={slideAnim.index === index + 1 ? "slide active-anim" : "slide"}
           >
-            <img
-              src={process.env.PUBLIC_URL + `/Imgs/img${index + 1}.jpg`}
-              alt="slide"
-            />
+            <img src={process.env.PUBLIC_URL + `/Imgs/img${index + 1}.jpg`} alt="" />
           </div>
-        );
+        )
       })}
-
       <BtnSlider moveSlide={nextSlide} direction={"next"} />
       <BtnSlider moveSlide={prevSlide} direction={"prev"} />
+
+
+      <div className="container-dots">
+        {Array.from({ length: 5 }).map((item, index) => {
+          return <div
+            className={slideAnim.index === index + 1 ? "dot active" : "dot"}
+            onClick={() => moveDot(index + 1)}
+          ></div>
+        })}
+      </div>
     </div>
-  );
+  )
 }

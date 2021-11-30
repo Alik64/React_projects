c4d368998327dac8bac480ec5f10fcd2b1f36f2d
Albert <albert.barsamov@gmail.com>
2021-11-30 22:48:57 +0100

    onChange={formik.handleChange} value={formik.values

--- 2_Formik/src/Components/Form.jsx
+++ 2_Formik/src/Components/Form.jsx
@@ -17,27 +17,28 @@ export default function Form() {
     return (
         <div className={styles.wrapper} >
 
+
             <form className={styles.form}>
 
                 <label htmlFor="chanel">Chanel</label>
-                <input type="text" id="chanel" name="chanel" />
+                <input type="text" id="chanel" name="chanel" onChange={formik.handleChange} value={formik.values.chanel} />
 
 
                 <label htmlFor="name">Name</label>
-                <input type="text" id="name" name="name" />
+                <input type="text" id="name" name="name" onChange={formik.handleChange} value={formik.values.name} />
 
 
 
                 <label htmlFor="email">Email</label>
-                <input type="email" id="email" name="email" />
+                <input type="email" id="email" name="email" onChange={formik.handleChange} value={formik.values.email} />
 
 
 
                 <label htmlFor="password">Password</label>
-                <input type="password" id="password" name="password" />
+                <input type="password" id="password" name="password" onChange={formik.handleChange} value={formik.values.password} />
 
 
-
+                <pre>{JSON.stringify(formik.values)}</pre>
                 <button>Submit</button>
             </form>
 

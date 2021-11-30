ac367716dea4244d64ea52b78ccf41960ae0ed0e
Albert <albert.barsamov@gmail.com>
2021-11-30 16:00:26 +0100

    import + initValues of useFormik

--- 2_Formik/src/Components/Form.jsx
+++ 2_Formik/src/Components/Form.jsx
@@ -1,28 +1,41 @@
 import React from 'react'
 import styles from './Form.module.css'
+
+import { useFormik } from 'formik'
+
+
+
 export default function Form() {
+    const formik = useFormik({
+        initialValues: {
+            chanel: "",
+            name: "",
+            email: "",
+            password: ""
+        }
+    })
     return (
         <div className={styles.wrapper} >
 
-            <form >
-                <div>
-                    <label htmlFor="chanel">Chanel</label>
-                    <input type="text" id="chanel" name="chanel" />
-                </div>
-                <div>
-                    <label htmlFor="name">Name</label>
-                    <input type="text" id="name" name="name" />
-                </div>
+            <form className={styles.form}>
 
-                <div>
-                    <label htmlFor="email">Email</label>
-                    <input type="email" id="email" name="email" />
-                </div>
+                <label htmlFor="chanel">Chanel</label>
+                <input type="text" id="chanel" name="chanel" />
 
-                <div>
-                    <label htmlFor="password">Password</label>
-                    <input type="password" id="password" name="password" />
-                </div>
+
+                <label htmlFor="name">Name</label>
+                <input type="text" id="name" name="name" />
+
+
+
+                <label htmlFor="email">Email</label>
+                <input type="email" id="email" name="email" />
+
+
+
+                <label htmlFor="password">Password</label>
+                <input type="password" id="password" name="password" />
+
 
 
                 <button>Submit</button>

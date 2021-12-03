3a50a2816279845806100051e6a44ff1d97290a6
Albert <albert.barsamov@gmail.com>
2021-12-03 23:33:00 +0100

    formik props

--- 2_Formik/src/Components/Formulaire.jsx
+++ 2_Formik/src/Components/Formulaire.jsx
@@ -29,11 +29,20 @@ const validationSchema = Yup.object({
     name: Yup.string().required('Required!'),
     email: Yup.string().email('Invalid email format').required('Required!'),
     password: Yup.string().required('Required!'),
+
     social: Yup.object({
         facebook: Yup.string().required('Required!'),
         twitter: Yup.string().required('Required twitter'),
     }),
 })
+
+const validateComments = value => {
+    let error
+    if (!value) {
+        error = "Required field"
+    }
+    return error
+}
 export default function Formulaire() {
 
 
@@ -44,100 +53,108 @@ export default function Formulaire() {
                 onSubmit={onSubmit}
                 validationSchema={validationSchema}
                 validateOnChange={false}
-                validateOnBlur={false}
+                validateOnBlur={true}
             >
-                <Form className={styles.form}>
+                {formik => {
+                    console.log("formik props", formik)
+                    return (
+                        <Form className={styles.form}>
 
-                    <div className={styles.form_champ}>
-                        <label htmlFor="name">Name</label>
-                        <Field type="text" id="name" name="name" />
-                        <ErrorMessage name="name" component={TextError} />
-                    </div>
+                            <div className={styles.form_champ}>
+                                <label htmlFor="name">Name</label>
+                                <Field type="text" id="name" name="name" />
+                                <ErrorMessage name="name" component={TextError} />
+                            </div>
 
-                    <div className={styles.form_champ}>
-                        <label htmlFor="email">Email</label>
-                        <Field type="email" id="email" name="email"
-                        />
-                        <ErrorMessage name="email" >
-                            {errMsg => <div className="error">{errMsg}</div>}
-                        </ErrorMessage>
+                            <div className={styles.form_champ}>
+                                <label htmlFor="email">Email</label>
+                                <Field type="email" id="email" name="email"
+                                />
+                                <ErrorMessage name="email" >
+                                    {errMsg => <div className="error">{errMsg}</div>}
+                                </ErrorMessage>
 
-                    </div>
+                            </div>
 
-                    <div className={styles.form_champ}>
-                        <label htmlFor="password">Password</label>
-                        <Field type="password" id="password" name="password"
-                        />
-                        <ErrorMessage name="password" component='div' className="error" />
-                    </div>
+                            <div className={styles.form_champ}>
+                                <label htmlFor="password">Password</label>
+                                <Field type="password" id="password" name="password"
+                                />
+                                <ErrorMessage name="password" component='div' className="error" />
+                            </div>
 
-                    <div className={styles.form_champ}>
-                        <Field id="comments" name="comments" as="textarea" placeholder="Enter your comment" />
-                    </div>
-                    <div className={styles.form_champ}>
-                        <label htmlFor="adress">Adress</label>
+                            <div className={styles.form_champ}>
+                                <Field id="comments" name="comments" as="textarea" placeholder="Enter your comment" validate={validateComments} />
+                                <ErrorMessage name="comments" component='div' className="error" />
 
-                        <FastField name="adress">
-                            {
-                                (props) => {
-                                    const { field, form, meta } = props
+                            </div>
+                            <div className={styles.form_champ}>
+                                <label htmlFor="adress">Adress</label>
 
-                                    return (
-                                        <div>
-                                            <input type="text" id="adress"  {...field} />
-                                            {meta.touched && meta.error ? <div>{meta.error}</div> : null}
-                                        </div>
-                                    )
-                                }
-                            }
-                        </FastField>
-                    </div>
-                    <div className={styles.form_champ}>
-                        <label htmlFor="facebook">Facebook profile</label>
-                        <Field id="facebook" name="social.facebook" type="text" />
-                        <ErrorMessage name="social.facebook" component='div' className="error" />
-                    </div>
-                    <div className={styles.form_champ}>
-                        <label htmlFor="twitter">Twitter profile</label>
-                        <Field id="twitter" name="social.twitter" type="text" />
+                                <FastField name="adress">
+                                    {
+                                        (props) => {
+                                            const { field, form, meta } = props
 
-                    </div>
-                    <div className={styles.form_champ}>
-                        <label htmlFor="telFix">Fix Tel Number</label>
-                        <Field id="telFix" name="phoneNumbers[0]" type="text" />
-                    </div>
-                    <div className={styles.form_champ}>
-                        <label htmlFor="telMobile">Mobile Number</label>
-                        <Field id="telMobile" name="phoneNumbers[1]" type="text" />
-                    </div>
-                    <div className={styles.form_champ}>
-                        <label htmlFor="">List of phone numbers</label>
-                        <FieldArray name='phNumber'>
-                            {
-                                (fieldArrayProps) => {
-                                    const { push, remove, form } = fieldArrayProps
-                                    const { values } = form
-                                    const { phNumber } = values
-                                    console.log('Form errors', form.errors)
-                                    return (
-                                        <div>
-                                            {phNumber.map((phNumber, index) => (
-                                                <div key={index}>
-                                                    <Field name={`phNumber[${index}]`}></Field>
-                                                    {index > 0 && <button type="button" onClick={() => remove(index)} >-</button>}
-
-                                                    <button type="button" onClick={() => push('')} >+</button>
+                                            return (
+                                                <div>
+                                                    <input type="text" id="adress"  {...field} />
+                                                    {meta.touched && meta.error ? <div>{meta.error}</div> : null}
                                                 </div>
-                                            ))}
-                                        </div>
-                                    )
-                                }
-                            }
-                        </FieldArray>
-                    </div>
+                                            )
+                                        }
+                                    }
+                                </FastField>
+                            </div>
+                            <div className={styles.form_champ}>
+                                <label htmlFor="facebook">Facebook profile</label>
+                                <Field id="facebook" name="social.facebook" type="text" />
+                                <ErrorMessage name="social.facebook" component='div' className="error" />
+                            </div>
+                            <div className={styles.form_champ}>
+                                <label htmlFor="twitter">Twitter profile</label>
+                                <Field id="twitter" name="social.twitter" type="text" />
 
-                    <button type="submit">Submit</button>
-                </Form>
+                            </div>
+                            <div className={styles.form_champ}>
+                                <label htmlFor="telFix">Fix Tel Number</label>
+                                <Field id="telFix" name="phoneNumbers[0]" type="text" />
+                            </div>
+                            <div className={styles.form_champ}>
+                                <label htmlFor="telMobile">Mobile Number</label>
+                                <Field id="telMobile" name="phoneNumbers[1]" type="text" />
+                            </div>
+                            <div className={styles.form_champ}>
+                                <label htmlFor="">List of phone numbers</label>
+                                <FieldArray name='phNumber'>
+                                    {
+                                        (fieldArrayProps) => {
+                                            const { push, remove, form } = fieldArrayProps
+                                            const { values } = form
+                                            const { phNumber } = values
+                                            console.log('Form errors', form.errors)
+                                            return (
+                                                <div>
+                                                    {phNumber.map((phNumber, index) => (
+                                                        <div key={index}>
+                                                            <Field name={`phNumber[${index}]`}></Field>
+                                                            {index > 0 && <button type="button" onClick={() => remove(index)} >-</button>}
+
+                                                            <button type="button" onClick={() => push('')} >+</button>
+                                                        </div>
+                                                    ))}
+                                                </div>
+                                            )
+                                        }
+                                    }
+                                </FieldArray>
+                            </div>
+
+                            <button type="submit">Submit</button>
+                        </Form>
+                    )
+                }}
+
 
 
             </Formik>

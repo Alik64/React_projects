f7dd7b9e0261f4a3e2ea2d234663ecca52148273
Albert <albert.barsamov@gmail.com>
2021-12-04 23:30:06 +0100

    Text area create

---
+++ 2_Formik/src/Components/FormControl/Textarea.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Field, ErrorMessage } from 'formik'
+import TextError from './TextError'
+
+function Textarea(props) {
+    const { name, label, ...rest } = props
+    return (
+        <div className="form-control">
+            <label htmlFor={name}>{label}</label>
+            <Field as='textarea' id={name} name={name} {...rest} />
+            <ErrorMessage name={name} component={TextError} />
+        </div>
+    )
+}
+
+export default Textarea

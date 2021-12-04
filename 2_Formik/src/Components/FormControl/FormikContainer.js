6a2c7f2e9e277f802d1ee57476977d49f6072b42
Albert <albert.barsamov@gmail.com>
2021-12-04 23:30:17 +0100

    Textarea

--- 2_Formik/src/Components/FormControl/FormikContainer.js
+++ 2_Formik/src/Components/FormControl/FormikContainer.js
@@ -5,10 +5,12 @@ import FormikControl from './FormikControl';
 
 export default function FormikContainer() {
     const initialValues = {
-        email: ""
+        email: "",
+        description: ""
     };
     const validationSchema = Yup.object({
-        email: Yup.string().required('Email is required').email('Invalid email format')
+        email: Yup.string().required('Email is required').email('Invalid email format'),
+        description: Yup.string().required('Required')
     })
     const onSubmit = values => console.log('Form data', values)
 
@@ -21,6 +23,7 @@ export default function FormikContainer() {
         >
             {formik => <Form>
                 <FormikControl control='input' type='email' label='Email' name='email' />
+                <FormikControl control='textarea' type='textarea' label='Description' name='description' />
                 <button type="submit">Submit</button>
             </Form>}
         </Formik>

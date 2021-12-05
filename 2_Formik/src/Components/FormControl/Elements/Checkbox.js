c32a0fb39bce724ab57d3c2ea4b3a9e0ccd81b78
Albert <albert.barsamov@gmail.com>
2021-12-05 17:37:26 +0100

    checkboxOption: [],

---
+++ 2_Formik/src/Components/FormControl/Elements/Checkbox.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Field, ErrorMessage } from 'formik'
+import TextError from '../TextError'
+
+function Checkbox() {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Checkbox

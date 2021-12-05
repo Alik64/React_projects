fdc25eb112ac57d914509c4630aeafe3369c8d2e
Albert <albert.barsamov@gmail.com>
2021-12-05 17:57:40 +0100

    checkbox gourp

--- 2_Formik/src/Components/FormControl/Elements/CheckboxGroup.js
+++ 2_Formik/src/Components/FormControl/Elements/CheckboxGroup.js
@@ -1,13 +1,39 @@
 import React from 'react'
+
 import { Field, ErrorMessage } from 'formik'
 import TextError from '../TextError'
 
-function CheckboxGroup() {
+
+function CheckboxGroup(props) {
+    const { label, name, options, ...rest } = props
     return (
-        <div>
-            hello
+        <div className="form-control">
+            <label>{label}</label>
+            <Field name={name} {...rest}>
+                {
+                    ({ field }) => {
+                        console.log('Field', field)
+                        return options.map(option => {
+                            return (<React.Fragment key={option.key}>
+                                <input
+                                    type="checkbox"
+                                    id={option.value}
+                                    {...field}
+                                    value={option.value}
+                                    checked={Boolean(field.value.includes(option.value))} />
+                                <label htmlFor="{option.value}">{option.key}</label>
+                            </React.Fragment>)
+
+                        })
+                    }
+                }
+            </Field>
+            <ErrorMessage name={name} component={TextError} />
+
         </div>
     )
 }
 
 export default CheckboxGroup
+
+

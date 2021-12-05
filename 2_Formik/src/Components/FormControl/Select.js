a5ad01b850ed6c417f9c079af64b65a09821656e
Albert <albert.barsamov@gmail.com>
2021-12-05 16:09:22 +0100

    <option key={option.value} value={option.value}>{option.key}</option>

--- 2_Formik/src/Components/FormControl/Select.js
+++ 2_Formik/src/Components/FormControl/Select.js
@@ -3,11 +3,16 @@ import { Field, ErrorMessage } from 'formik'
 import TextError from './TextError'
 
 function Select(props) {
-    const { name, label, ...rest } = props
+    const { name, label, options, ...rest } = props
     return (
         <div className="form-control">
             <label htmlFor={name}>{label}</label>
-            <Field as="select" id={name} name={name} />
+            <Field as="select" id={name} name={name} {...rest}>
+                {options.map(option => {
+                    <option key={option.value} value={option.value}>{option.key}</option>
+                })}
+            </Field>
+            <ErrorMessage name={name} component={TextError} />
         </div>
     )
 }

3b67db09f2b73f041467dffd2c8808165a6a395d
Albert <albert.barsamov@gmail.com>
2021-12-05 16:59:32 +0100

    RadioButtons component

--- 2_Formik/src/Components/FormControl/Elements/RadioButtons.js
+++ 2_Formik/src/Components/FormControl/Elements/RadioButtons.js
@@ -14,13 +14,20 @@ function RadioButtons() {
                     ({ field }) => {
                         return options.map(option => {
                             <Fragment key={option.key}>
-                                <input type="radio" id={option.value} {...field} value={option.value} />
+                                <input
+                                    type="radio"
+                                    id={option.value}
+                                    {...field}
+                                    value={option.value}
+                                    checked={field.value === option.value} />
+                                <label htmlFor="{option.value}">{option.value}</label>
                             </Fragment>
                         })
                     }
                 }
-
             </Field>
+            <ErrorMessage name={name} component={TextError} />
+
         </div>
     )
 }

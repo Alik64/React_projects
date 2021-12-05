58e4862be6a5f7e46f51e1cba93261dc006aa916
Albert <albert.barsamov@gmail.com>
2021-12-05 16:50:16 +0100

    Fragment

--- 2_Formik/src/Components/FormControl/Elements/RadioButtons.js
+++ 2_Formik/src/Components/FormControl/Elements/RadioButtons.js
@@ -1,13 +1,26 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 
 import { Field, ErrorMessage } from 'formik'
 import TextError from '../TextError'
+
 
 function RadioButtons() {
     const { label, name, options, ...rest } = props
     return (
         <div className="form-control">
-            <label htmlFor="">{label}</label>
+            <label>{label}</label>
+            <Field name={name} {...rest}>
+                {
+                    ({ field }) => {
+                        return options.map(option => {
+                            <Fragment key={option.key}>
+                                <input type="radio" id={option.value} {...field} value={option.value} />
+                            </Fragment>
+                        })
+                    }
+                }
+
+            </Field>
         </div>
     )
 }

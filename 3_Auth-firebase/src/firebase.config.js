f92fcdf1560e8a7e6fe11c0b4cca078696fd6576
Albert <albert.barsamov@gmail.com>
2021-12-10 15:28:46 +0100

    ajout firebase.config.js

---
+++ 3_Auth-firebase/src/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyB0_Hhbg_PJRx6FqqxgOtoWK-wrm8KA-gw",
+    authDomain: "react-auth-routerv6.firebaseapp.com",
+    projectId: "react-auth-routerv6",
+    storageBucket: "react-auth-routerv6.appspot.com",
+    messagingSenderId: "789341260566",
+    appId: "1:789341260566:web:09f9d0e7d636e8d5f91e37"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);

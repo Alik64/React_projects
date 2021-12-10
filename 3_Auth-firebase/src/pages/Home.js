db871b1072513c53c272f83a3be71848900aba4f
Albert <albert.barsamov@gmail.com>
2021-12-10 16:15:32 +0100

    creation - page home

--- 3_Auth-firebase/src/pages/Home.js
+++ 3_Auth-firebase/src/pages/Home.js
@@ -2,8 +2,9 @@ import React from 'react'
 
 export default function Home() {
     return (
-        <div>
-            <h1>Hello</h1>
+        <div className="container p-5">
+            <h1 className="display-2 text-light ">Home</h1>
+            <h1 className="display-4 text-light">Hello, Sign up or Sign in</h1>
         </div>
     )
 }

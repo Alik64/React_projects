23797b250f5ad3fa9b765f948b99b60e3a3c5262
Albert <albert.barsamov@gmail.com>
2021-12-11 15:36:19 +0100

    ajout sign up form

--- 3_Auth-firebase/src/App.js
+++ 3_Auth-firebase/src/App.js
@@ -1,11 +1,13 @@
 
 import { Routes, Route } from "react-router-dom"
 import Navbar from "./components/Navbar";
+import SignUpModal from "./components/SignUpModal";
 import Home from "./pages/Home"
 function App() {
   return (
     <>
       <Navbar />
+      <SignUpModal />
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
---
+++ 3_Auth-firebase/src/components/SignUpModal.js
@@ -0,0 +1,60 @@
+import React from 'react'
+
+export default function SignUpModal() {
+
+    return (
+        <>
+            <div className="position-fixed top-0 vw-100 vh-100">
+                <div className="w-100 h-100 bg-dark bg-opacity-75">
+                    <div className="position-absolute top-50 start-50 translate-middle" style={{ minWidth: "400px" }}>
+                        <div className="modal-dialog">
+                            <div className="modal-content">
+                                <div className="modal-header">
+                                    <h5 className="modal-title">Sign Up</h5>
+                                    <button className="btn-close"></button>
+                                </div>
+
+                                <div className="modal-body">
+
+                                    <form className="sign-up-form">
+                                        <div className="mb-3">
+                                            <label htmlFor="signUpEmail" className='form-label'>Email adress</label>
+                                            <input
+                                                required
+                                                name="email"
+                                                type="email"
+                                                id="signUpEmail"
+                                                className="form-control" />
+                                        </div>
+                                        <div className="mb-3">
+                                            <label htmlFor="signUpPwd" className='form-label'>Password</label>
+                                            <input
+                                                required
+                                                name="pwd"
+                                                type="password"
+                                                id="signUpPwd"
+                                                className="form-control" />
+                                        </div>
+                                        <div className="mb-3">
+                                            <label htmlFor="repeatPwd" className='form-label'>Repeat password</label>
+                                            <input
+                                                required
+                                                name="pwd"
+                                                type="password"
+                                                id="repeatPwd"
+                                                className="form-control" />
+                                            <p className="text-danger mt-1">Validation</p>
+                                        </div>
+                                        <button className='btn btn-primary'>Submit</button>
+                                    </form>
+
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}

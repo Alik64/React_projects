2607aec36e20e0ff9d31a16d4b53116089b38d6a
Albert <albert.barsamov@gmail.com>
2021-12-12 23:56:46 +0100

    Personnaliser message Home en fonction de login

--- 3_Auth-firebase/src/pages/Home.js
+++ 3_Auth-firebase/src/pages/Home.js
@@ -1,10 +1,15 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { UserContext } from '../context/userContext'
 
 export default function Home() {
+
+    const { currentUser } = useContext(UserContext)
+
     return (
         <div className="container p-5">
             <h1 className="display-2 text-light ">Home</h1>
-            <h1 className="display-4 text-light">Hello, Sign up or Sign in</h1>
+            {currentUser ? <h2 className='text-light' >Hello Buddy!</h2> : <h2 className='text-light'>Hello, Stranger! Sign up or Sign in</h2>}
+
         </div>
     )
 }

0fe157e3d8a9f29a42dc4ffcc98236e69f687f70
Albert <albert.barsamov@gmail.com>
2021-12-12 23:44:57 +0100

    ajout LogOut

--- 3_Auth-firebase/src/components/Navbar.js
+++ 3_Auth-firebase/src/components/Navbar.js
@@ -1,11 +1,25 @@
 import React, { useContext } from 'react'
 import { Link } from 'react-router-dom'
 import { UserContext } from '../context/userContext'
+import { signOut } from 'firebase/auth'
+import { useNavigate } from 'react-router-dom'
+import { auth } from '../firebase-config'
+
+
 
 export default function Navbar() {
     const { toggleModals } = useContext(UserContext)
+    const navigate = useNavigate()
+    const logOut = async () => {
 
+        try {
+            await signOut(auth)
+            navigate("/")
+        } catch {
+            alert("Impossible to deconnect, please check your internet connexion and retry")
+        }
 
+    }
 
     return (
         <nav className="navbar navbar-light bg-light px-4">
@@ -16,7 +30,7 @@ export default function Navbar() {
             <div>
                 <button onClick={() => toggleModals('signUp')} className="btn btn-primary">Sign Up</button>
                 <button onClick={() => toggleModals('signIn')} className="btn btn-primary ms-2">Sign In</button>
-                <button className="btn btn-danger ms-2">Log out</button>
+                <button onClick={logOut} className="btn btn-danger ms-2">Log out</button>
             </div>
         </nav>
     )

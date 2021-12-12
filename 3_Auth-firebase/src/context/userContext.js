50215b52f7746a7afb1d5fb2c925d887b6eab686
Albert <albert.barsamov@gmail.com>
2021-12-12 21:24:31 +0100

    import {     signInWithEmailAndPassword,     createUserWithEmailAndPassword,     onAuthStateChanged

--- 3_Auth-firebase/src/context/userContext.js
+++ 3_Auth-firebase/src/context/userContext.js
@@ -1,4 +1,12 @@
 import { createContext, useState, useEffect } from "react";
+import {
+    signInWithEmailAndPassword,
+    createUserWithEmailAndPassword,
+    onAuthStateChanged
+} from 'firebase/auth';
+import { auth } from '../firebase-config'
+
+
 
 export const UserContext = createContext()
 

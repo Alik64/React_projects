2d12c9ed62671313d058feed22cbff17dd7a88a5
Albert <albert.barsamov@gmail.com>
2021-12-16 00:17:20 +0100

    ajout constructor au class SQUARE

--- 04_react-docs/src/index.js
+++ 04_react-docs/src/index.js
@@ -4,6 +4,14 @@ import './index.css';
 
 
 class Square extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: null,
+    }
+  }
+
+
   render() {
     return (
       <button className="square" onClick={() => { alert('click') }}>

1bf45588399f7d672d8184c76e3bf00c00dce892
Albert <albert.barsamov@gmail.com>
2021-12-16 11:49:27 +0100

    4 : rempalcer claass par fonction Square

--- 4_react-docs/src/index.js
+++ 4_react-docs/src/index.js
@@ -3,24 +3,25 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 
-class Square extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: null,
-    }
-  }
+// class Square extends React.Component {
 
+//   render() {
+//     return (
+//       <button
+//         className="square"
+//         onClick={() => { this.props.onClick() }}>
+//         {this.props.value}
+//       </button>
+//     );
+//   }
+// }
 
-  render() {
-    return (
-      <button
-        className="square"
-        onClick={() => { this.setState({ value: 'X' }) }}>
-        {this.state.value}
-      </button>
-    );
-  }
+function Square(props) {
+  return (
+    <button className='square'
+      onClick={() => { props.onClick() }}
+    >{props.value}</button>
+  )
 }
 
 class Board extends React.Component {
@@ -30,10 +31,16 @@ class Board extends React.Component {
       squares: Array(9).fill(null)
     }
   }
-
+  handleClick(i) {
+    const squares = [...this.state.squares]
+    squares[i] = 'X'
+    this.setState({ squares: squares })
+  }
 
   renderSquare(i) {
-    return <Square value={i} />;
+    return <Square
+      onClick={() => this.handleClick(i)}
+      value={this.state.squares[i]} />;
   }
 
   render() {

32d0b708b7c44b8bc6e8a5bb99859e21c9027417
Albert <albert.barsamov@gmail.com>
2021-12-17 00:09:31 +0100

    4: const history = this.state.history.slice(0, this.state.stepNumber + 1)

--- 4_react-docs/src/index.js
+++ 4_react-docs/src/index.js
@@ -50,12 +50,13 @@ class Game extends React.Component {
       history: [{
         squares: Array(9).fill(null)
       }],
-      xIsNext: true
+      xIsNext: true,
+      stepNumber: 0,
     };
   }
 
   handleClick(i) {
-    const history = this.state.history;
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
     const current = history[history.length - 1];
     const squares = current.squares.slice();
     if (calculateWinner(squares) || squares[i]) {
@@ -66,10 +67,17 @@ class Game extends React.Component {
       history: history.concat([{
         squares: squares
       }]),
+      stepNumber: history.length,
       xIsNext: !this.state.xIsNext,
     });
   }
 
+  jumpTo(step) {
+    this.setState({
+      stepNumber: step,
+      xIsNext: (step % 2) === 0,
+    });
+  }
   render() {
     const history = this.state.history;
     const current = history[history.length - 1];

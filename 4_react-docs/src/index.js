368f669025ded1e48c4047ff1f02dd9fcc996084
Albert <albert.barsamov@gmail.com>
2021-12-16 15:18:46 +0100

    4: Map pour l'historique

--- 4_react-docs/src/index.js
+++ 4_react-docs/src/index.js
@@ -74,7 +74,16 @@ class Game extends React.Component {
     const history = this.state.history;
     const current = history[history.length - 1];
     const winner = calculateWinner(current.squares);
-
+    const moves = history.map((step, move) => {
+      const desc = move ?
+        'Revenir au tour n°' + move :
+        'Revenir au début de la partie';
+      return (
+        <li key={move}>
+          <button onClick={() => this.jumpTo(move)}>{desc}</button>
+        </li>
+      );
+    });
     let status;
     if (winner) {
       status = 'Winner: ' + winner;
@@ -92,7 +101,7 @@ class Game extends React.Component {
         </div>
         <div className="game-info">
           <div>{status}</div>
-          <ol>{/* TODO */}</ol>
+          <ol>{moves}</ol>
         </div>
       </div>
     );

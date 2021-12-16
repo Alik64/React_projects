f2c3d66bb2bea669798fa4cb2a1f0c9e424b74a3
Albert <albert.barsamov@gmail.com>
2021-12-17 00:13:34 +0100

    4: const current = history[this.state.stepNumber]

--- 4_react-docs/src/index.js
+++ 4_react-docs/src/index.js
@@ -80,7 +80,7 @@ class Game extends React.Component {
   }
   render() {
     const history = this.state.history;
-    const current = history[history.length - 1];
+    const current = history[this.state.stepNumber];
     const winner = calculateWinner(current.squares);
     const moves = history.map((step, move) => {
       const desc = move ?

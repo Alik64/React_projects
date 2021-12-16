0f78911921bff47eb8954bfd10c0b45bab6e9dc7
Albert <albert.barsamov@gmail.com>
2021-12-16 12:43:49 +0100

    4: ajout disable Square

--- 4_react-docs/src/index.js
+++ 4_react-docs/src/index.js
@@ -21,6 +21,9 @@ class Board extends React.Component {
   }
   handleClick(i) {
     const squares = [...this.state.squares]
+    if (this.calculateWinner(squares) || squares[i]) {
+      return;
+    }
     squares[i] = this.state.xIsNext ? 'X' : "O"
     this.setState({
       squares: squares,
@@ -36,8 +39,29 @@ class Board extends React.Component {
       value={this.state.squares[i]} />;
   }
 
+  calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
   render() {
-    const winner = calculateWinner(this.state.squares);
+
+
+    const winner = this.calculateWinner(this.state.squares);
     let status;
     if (winner) {
       status = winner + ' a gagné';
@@ -90,22 +114,4 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
-    }
-  }
-  return null;
-}
+
